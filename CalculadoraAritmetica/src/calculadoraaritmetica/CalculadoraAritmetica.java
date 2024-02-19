704fe3cc1dcaf848d8d528e20a70b5b63e7bf93c
OsunaGitHub <149728691+Ozutochi@users.noreply.github.com>
2024-02-19 07:14:31 +0100

    Add files via upload

---
+++ CalculadoraAritmetica/src/calculadoraaritmetica/CalculadoraAritmetica.java
@@ -0,0 +1,25 @@
+
+package calculadoraaritmetica;
+
+import java.io.IOException;
+
+
+public class CalculadoraAritmetica{
+    
+    public static void main (String [] args) throws IOException{
+   
+    NPI prueba = new NPI();
+  
+     prueba.leerArchivo();
+   
+   
+   
+       
+    }
+
+
+
+
+
+
+}
